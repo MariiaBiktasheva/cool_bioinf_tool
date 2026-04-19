71837fc44d78810d97af2d2a78bccd7f3aa325b1
Mariia Biktasheva <marybikt@gmail.com>
2026-04-19 18:50:08 +0300

    Remove legacy

--- modules_filter_fastq/reverse_complement_module.py
+++
@@ -1,7 +0,0 @@
-# Add reverse_complement
-def reverse_complement(sequence):
-    """
-    Returns a reverse complementary sequence:
-    attention!!! It only returns DNA
-    """
-    return complement(reverse(sequence))
--- modules_filter_fastq/reverse_module.py
+++
@@ -1,8 +0,0 @@
-# Add reverse
-def reverse(sequence):
-    """
-    Reverse sequence
-    """
-    new_seq = sequence[::-1]
-    return new_seq
-
